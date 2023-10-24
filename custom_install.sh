e44ef4dbbda4d8b093878be062e614395f7844c7
SDJustus <44165116+SDJustus@users.noreply.github.com>
2023-10-24 10:25:34 +0200

    Update custom_install.sh

--- custom_install.sh
+++ custom_install.sh
@@ -6,4 +6,4 @@ set -o errexit
 set -o errtrace
 
 # Install dotfiles
-~/.bash_it/install.sh --silent --append-to-config && source ~/.bashrc && bash_it enable aliases git kubectl
+~/.bash_it/install.sh --silent --append-to-config && source ~/.bashrc && bash-it enable aliases git kubectl
